be899ef2a00d41f362dafb254db769da552b1a6c
azimjonalijonov <azimjonalijonoff@gmail.com>
2024-03-22 15:51:57 +0500

    converted end to end tests to unit tests

--- trainer-summary-service/src/test/java/org/example/service/cucumberTests/TrainerSummaryControllerTest.java
+++ trainer-summary-service/src/test/java/org/example/service/cucumberTests/TrainerSummaryControllerTest.java
@@ -3,12 +3,23 @@ package org.example.service.cucumberTests;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.example.TrainerSummary;
+import org.example.controller.TrainerSummaryController;
+import org.example.dto.ResponseDto;
+import org.example.service.TrainerSummaryService;
 import org.junit.Assert;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 
 public class TrainerSummaryControllerTest {
     private String username;
@@ -16,6 +27,14 @@ public class TrainerSummaryControllerTest {
     private RestTemplate restTemplate = new RestTemplate();
 
     private ResponseEntity<?> response;
+    @Mock
+    TrainerSummaryService trainerSummaryService;
+    @InjectMocks
+    TrainerSummaryController trainerSummaryController;
+
+    public TrainerSummaryControllerTest() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Given("a trainer with username {string} exists")
     public void a_trainer_with_username_exists(String trainerUsername) {
@@ -23,13 +42,11 @@ public class TrainerSummaryControllerTest {
     }
 
     @When("a request is made to retrieve the trainer summary")
-    public void a_request_is_made_to_retrieve_the_trainer_summary_for_username() {
-        HttpHeaders headers =new HttpHeaders();
-        headers.set("Content-type","Application/json");
-        headers.set("Authorization","Bearer "+token);
-        HttpEntity<String> httpEntity =new HttpEntity<>(headers);
-        response =restTemplate.exchange("http://localhost:9090/api/get?username="+username, HttpMethod.GET,httpEntity,String.class);
-    }
+    public void a_request_is_made_to_retrieve_the_trainer_summary_for_username() throws Exception {
+        ResponseDto responseDto =new ResponseDto();
+        Mockito.when(trainerSummaryService.get(anyString())).thenReturn(responseDto);
+        response=trainerSummaryController.get(username);
+       }
 
     @Then("the trainer summary should be successfully retrieved")
     public void the_trainer_summary_should_be_successfully_retrieved() {
